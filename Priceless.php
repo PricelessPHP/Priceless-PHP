2ba7b1f14b5329c93613f5ae0ce7dd758ba9448e
MarQuisKnox <MarQuisKnox@users.noreply.github.com>
2015-02-28 11:43:08 +0100

    Update Priceless.php

--- Priceless.php
+++ Priceless.php
@@ -6,6 +6,7 @@
  * @author      MarQuis Knox <opensource@marquisknox.com>
  * @copyright   2015 MarQuis Knox
  * @link        http://marquisknox.com
+ * @link        http://pricelessphp.com
  * @license     Affero General Public License v3
  *
  * @since  	    Wednesday, February 04, 2015, 16:25 GMT+1
